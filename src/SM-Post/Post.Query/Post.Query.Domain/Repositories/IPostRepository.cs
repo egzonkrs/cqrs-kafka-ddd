0c97299af492e512ad5f035b11838baff564ce08
egzonkrs <71973901+egzonkrs@users.noreply.github.com>
2022-09-20 22:38:42 +0200

    feat: added IPostRepository and ICommentRepository interfaces

---
+++ src/SM-Post/Post.Query/Post.Query.Domain/Repositories/IPostRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Post.Query.Domain.Entities;
+
+namespace Post.Query.Domain.Repositories;
+
+public interface IPostRepository
+{
+    Task CreateAsync(PostEntity post);
+    Task UpdateAsync(PostEntity post);
+    Task DeleteAsync(Guid postId);
+    Task<PostEntity> GetByIdAsync(Guid postId);
+    Task<List<PostEntity>> ListAllAsync();
+    Task<List<PostEntity>> ListByAuthorAsync(string author);
+    Task<List<PostEntity>> ListWithLikes(int numberOfLikes);
+    Task<List<PostEntity>> ListWithCommentsAsync();
+}
